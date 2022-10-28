bca30b00a3abad5828ade7697eb63af8dc292746
Martin Saxtorff <masa50358@edu.ucl.dk>
2022-10-28 09:57:06 +0200

    Label form1 tilføjet

--- GitHubTest/Form1.Designer.cs
+++ GitHubTest/Form1.Designer.cs
@@ -29,13 +29,34 @@ namespace GitHubTest
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lb_sfl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb_sfl
+            // 
+            this.lb_sfl.AutoSize = true;
+            this.lb_sfl.Location = new System.Drawing.Point(341, 216);
+            this.lb_sfl.Name = "lb_sfl";
+            this.lb_sfl.Size = new System.Drawing.Size(92, 17);
+            this.lb_sfl.TabIndex = 0;
+            this.lb_sfl.Text = "Stor fed label";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lb_sfl);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lb_sfl;
     }
 }
 
